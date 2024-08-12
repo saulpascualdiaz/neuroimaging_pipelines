8f847904bb9595c2acada9d71e9020842c76c2a8
Saül Pascual-Diaz <44832858+saulpascualdiaz@users.noreply.github.com>
2024-08-12 14:30:56 +0200

    GNU parallel function added into dwi_preprocessing_appa_pipeline.sh

--- scripts/dwi_preprocessing_appa_pipeline.sh
+++ scripts/dwi_preprocessing_appa_pipeline.sh
@@ -7,45 +7,43 @@
 # This script processes diffusion-weighted imaging (DWI) data for multiple subjects
 # following the Brain Imaging Data Structure (BIDS) format. It checks for the presence
 # of necessary input files, performs denoising, and runs the FSL pre-processing pipeline.
+# The script utilizes GNU `parallel` to process multiple subjects in parallel, leveraging 
+# the processing power of multi-core systems.
 
 # Assumptions:
 # - The BIDS directory structure follows the standard format.
-# - The script is run in an environment where all necessary tools (dwidenoise, fslroi, mrcat, dwifslpreproc) are available.
+# - The script is run in an environment where all necessary tools (dwidenoise, fslroi, 
+#   mrcat, dwifslpreproc, mrdegibbs) are available.
+# - GNU `parallel` is installed and available in your system's PATH.
 
-# Example BIDS Directory Structure:
-# bids_data/
-# ├── sub-01/
-# │   └── ses-baseline/
-# │       └── dwi/
-# │           ├── sub-01_ses-baseline_dir-AP_dwi.bval
-# │           ├── sub-01_ses-baseline_dir-AP_dwi.bvec
-# │           ├── sub-01_ses-baseline_dir-AP_dwi.nii.gz
-# │           ├── sub-01_ses-baseline_dir-PA_dwi.bval
-# │           ├── sub-01_ses-baseline_dir-PA_dwi.bvec
-# │           └── sub-01_ses-baseline_dir-PA_dwi.nii.gz
-# └── sub-02/
-#     └── ses-baseline/
-#         └── dwi/
-#             ├── sub-02_ses-baseline_dir-AP_dwi.bval
-#             ├── sub-02_ses-baseline_dir-AP_dwi.bvec
-#             ├── sub-02_ses-baseline_dir-AP_dwi.nii.gz
-#             ├── sub-02_ses-baseline_dir-PA_dwi.bval
-#             ├── sub-02_ses-baseline_dir-PA_dwi.bvec
-#             └── sub-02_ses-baseline_dir-PA_dwi.nii.gz
+# Installation of GNU `parallel`:
+# - On macOS, you can install `parallel` using Homebrew. First, install Homebrew 
+#   if you don't have it:
+#     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# - Then, install `parallel` using the following command:
+#     brew install parallel
+#
+# Usage:
+# - This script uses `parallel` to run the DWI processing pipeline on multiple subjects 
+#   simultaneously. You can configure the number of parallel jobs by setting the `parallel_jobs` 
+#   variable below. Ensure that the total number of threads used by all parallel jobs does not exceed 
+#   the number of available CPU cores.
 
 # Configuration
 bids_dir=/Volumes/working_disk_blue/SPRINT_MPS/bids_data
 bids_out=/Volumes/working_disk_blue/SPRINT_MPS/bids_derivatives/DWI_postprocessed
-threads=10
+parallel_jobs=8  # Set the number of parallel jobs (subjects) to process simultaneously
 
 # Do not modify beyond this point ------------------------------------------------
 # ANSI color codes
 BLUE='\033[0;34m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
 NC='\033[0m' # No Color
 
-for s in $(ls ${bids_dir}); do
+process_subject() {
+    s=$1
     printf "${BLUE}Working in subject ${s}...${NC}\n"
 
     # Basic variables
@@ -55,8 +53,8 @@ for s in $(ls ${bids_dir}); do
     start_time=$(date +%s)
     
     if [ -f ${od}-AP_dwi_corr.nii.gz ]; then
-        printf "${GREEN}Subject ${s} already pre-processed.${NC}\n"
-        continue
+        echo "${GREEN}Subject ${s} already pre-processed.${NC}"
+        return
     fi
 
     # Checking for input files
@@ -70,7 +68,7 @@ for s in $(ls ${bids_dir}); do
     done
     
     if [ "$missing_files" = true ]; then
-        continue
+        return
     fi
     
     if [ ! -d ${bids_out}/${s}/ses-${ses} ]; then
@@ -79,11 +77,12 @@ for s in $(ls ${bids_dir}); do
     
     # DWI denoising
     if [ ! -f ${od}-AP_dwi_denoised.nii.gz ]; then
-        dwidenoise ${wd}-AP_dwi.nii.gz ${od}-AP_dwi_denoised.nii.gz -nthreads ${threads}
+        dwidenoise ${wd}-AP_dwi.nii.gz ${od}-AP_dwi_denoised.nii.gz
         if [ $? -eq 0 ]; then
             printf "${GREEN}Denoising completed for subject ${s}${NC}\n"
         else
             printf "${RED}[ERROR] Denoising failed for subject ${s}${NC}\n"
+            return
         fi
     else
         printf "${ORANGE}Denoised file already exists for subject ${s}, skipping denoising${NC}\n"
@@ -91,7 +90,7 @@ for s in $(ls ${bids_dir}); do
     
     # Step 2: Remove Gibbs ringing
     if [ ! -f ${od}-AP_dwi_unringed.nii.gz ]; then
-        mrdegibbs ${od}-AP_dwi_denoised.nii.gz  ${od}-AP_dwi_unringed.nii.gz -nthreads ${threads}
+        mrdegibbs ${od}-AP_dwi_denoised.nii.gz  ${od}-AP_dwi_unringed.nii.gz
         if [ $? -eq 0 ]; then
             printf "${GREEN}Gibbs ringing removal completed for subject ${s}${NC}\n"
         else
@@ -110,7 +109,7 @@ for s in $(ls ${bids_dir}); do
             ${od}-AP_dwi_corr.nii.gz -fslgrad ${wd}-AP_dwi.bvec ${wd}-AP_dwi.bval \
             -export_grad_fsl ${od}-AP_dwi_corr.bvec ${od}-AP_dwi_corr.bval \
             -rpe_pair -se_epi ${od}-b0_pair.mif  -eddy_options " --slm=linear "\
-            -pe_dir ap -align_seepi -nthreads ${threads}
+            -pe_dir ap -align_seepi
         if [ $? -eq 0 ]; then
             printf "${GREEN}FSL pre-processing completed for subject ${s}${NC}\n"
         else
@@ -135,5 +134,11 @@ for s in $(ls ${bids_dir}); do
     else
         echo -e "${BLUE}Subject ${s} processing time: $duration seconds.${NC}\n"
     fi
-    break
-done
+}
+
+export bids_dir bids_out BLUE RED GREEN ORANGE NC
+export -f process_subject
+
+# Execute the processing function in parallel for each subject in the bids_dir
+# The parallel_jobs variable controls the number of parallel jobs (subjects) to process simultaneously.
+ls ${bids_dir} | parallel -j ${parallel_jobs} process_subject {}
